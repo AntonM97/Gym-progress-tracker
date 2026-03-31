89ea0e6efb1b3cd805b27b3fdb3285481fa866dd
Anton <anton.matsibora@gmail.com>
2026-04-01 01:02:18 +0200

    fixed record service

--- src/main/java/cc/hiknomore/gym_progress/service/RecordService.java
+++ src/main/java/cc/hiknomore/gym_progress/service/RecordService.java
@@ -1,12 +1,26 @@
 package cc.hiknomore.gym_progress.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import cc.hiknomore.gym_progress.model.Record;
+
+import cc.hiknomore.gym_progress.repository.RecordRepository;
 
 @Service
 public class RecordService {
 
-    public RecordService() {
+    RecordRepository recordRepository;
+
+    public RecordService(RecordRepository recordRepository) {
+        this.recordRepository = recordRepository;
     }
-    
-    
+
+    public List<Record> getWeeklyRecords(Long id) {
+        LocalDate from = LocalDate.now().minusDays(7);
+        LocalDate to = LocalDate.now();
+        return recordRepository.findByDateBetween(id, from, to);
+    }
+
 }
