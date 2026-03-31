ea28c1789feab459674a38bad3a45f3d3254c4e1
Anton <anton.matsibora@gmail.com>
2026-04-01 01:02:34 +0200

    fixed request in RecordRepository

--- src/main/java/cc/hiknomore/gym_progress/repository/RecordRepository.java
+++ src/main/java/cc/hiknomore/gym_progress/repository/RecordRepository.java
@@ -1,9 +1,17 @@
 package cc.hiknomore.gym_progress.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 import java.util.List;
 import cc.hiknomore.gym_progress.model.Record;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
+    
     List<Record> findByPersonId(Long personId);
+    
+    //TODO 
+    @Query("select r from Record r where r.person.id =:id and r.date between :from and :to")
+    List<Record> findByDateBetween(Long id, LocalDate from, LocalDate to);
 }
