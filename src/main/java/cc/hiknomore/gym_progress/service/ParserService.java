0f1714c4edb6970b95a4f7dd96d27ddebc1e9b2f
Anton <anton.matsibora@gmail.com>
2026-03-31 11:03:44 +0200

    started creating service layer

---
+++ src/main/java/cc/hiknomore/gym_progress/service/ParserService.java
@@ -0,0 +1,14 @@
+package cc.hiknomore.gym_progress.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParserService {
+    
+    public ParserService() {}
+    
+    public void parseTextInput() {
+        
+    }
+
+}
---
+++ src/main/java/cc/hiknomore/gym_progress/service/RecordService.java
@@ -0,0 +1,12 @@
+package cc.hiknomore.gym_progress.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecordService {
+
+    public RecordService() {
+    }
+    
+    
+}
