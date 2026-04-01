cf158e6f94a223df2edc29393800b4e50e2af0af
foxstudent100683 <anton.matsibora@gmail.com>
2026-04-01 19:16:11 +0200

    update for controllers(actually have to move to other pc)

--- src/main/java/cc/hiknomore/gym_progress/controller/JustDoItController.java
+++ src/main/java/cc/hiknomore/gym_progress/controller/JustDoItController.java
@@ -1,8 +1,12 @@
 package cc.hiknomore.gym_progress.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping()
 public class JustDoItController {
 
+    
+    
 }
--- src/main/java/cc/hiknomore/gym_progress/controller/StatsController.java
+++ src/main/java/cc/hiknomore/gym_progress/controller/StatsController.java
@@ -1,8 +1,10 @@
 package cc.hiknomore.gym_progress.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping
 public class StatsController {
 
 }
