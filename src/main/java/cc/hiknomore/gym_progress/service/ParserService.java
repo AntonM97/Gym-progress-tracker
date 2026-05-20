686751fd135dad967f33149d0d8e038f2535e142
foxstudent100683 <anton.matsibora@gmail.com>
2026-05-20 17:29:29 +0200

    started work on a parser service

--- src/main/java/cc/hiknomore/gym_progress/service/ParserService.java
+++ src/main/java/cc/hiknomore/gym_progress/service/ParserService.java
@@ -1,5 +1,8 @@
 package cc.hiknomore.gym_progress.service;
 
+import java.util.regex.Pattern;
+
+import org.apache.logging.log4j.message.StringFormattedMessage;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,8 +10,18 @@ public class ParserService {
     
     public ParserService() {}
     
-    public void parseTextInput() {
+    public void parseTextInput(String input) {
+        input.trim();
+        Pattern time = Pattern.compile("");//TODO
+        Pattern timeSets = Pattern.compile("");//TODO
+        Pattern timeDistance = Pattern.compile("");//TODO
+        Pattern timeWeight = Pattern.compile("");//TODO
         
+        Pattern repsSets = Pattern.compile("");//TODO
+        Pattern repsPerSet = Pattern.compile("");//TODO
+        Pattern repsSetsWeight = Pattern.compile("");//TODO
+        
+        Pattern distance = Pattern.compile("");//TODO
     }
 
 }
